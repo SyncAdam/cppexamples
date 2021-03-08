5a82a40ec67c938f7bd86810cfd1f856ec4d54c5
SyncAdam <61192656+SyncAdam@users.noreply.github.com>
2021-03-08 11:21:34 +0100

    Update main.cpp

--- exceptionhandling/logicerr/main.cpp
+++ exceptionhandling/logicerr/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+//
+//	Logic error exemple -=- length_error -=-
+//
+
 int main(){
 
+	vector<int> mynums;
 
-  return 0;
+	try{
+
+		mynums.resize(mynums.max_size() + 1);
+	
+	}
+	catch(const length_error& err){
+
+		cout << err.what() << endl ;
+		cout  << mynums.max_size() << endl;
+
+	}
+	
+
+	return 0;
 }
